573bbb1389bbb6f87fe5517c4e6e2ec3158e2c07
QuantumPickleJar <vmorrilll@gmail.com>
2018-09-12 20:04:54 -0500

    Added everything

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    { 
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            string strGreet = "Hello World!";
+            string strBye = "Goodbye World!";
+
+            //DEBUG lblHello.Text = lblHello.Text.CompareTo(strGreet);
+
+            //if the text already displayed is strGreet, 
+            //swap text property to strBye...
+
+
+            if (lblHello.Text.CompareTo(strGreet) == 0)
+            {
+                lblHello.Text = strBye;
+            }
+
+            //... and vice versa
+            else if (lblHello.Text.CompareTo(strBye) == 0)
+            {
+                lblHello.Text = strGreet;
+            }
+            else lblHello.Text = "Somehow, something managed to go wrong.";
+
+
+
+        }
+    }
+}
